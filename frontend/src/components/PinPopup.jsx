d3a2c1eec8a8932c25b0449f64afbc6c80ceff04
riyaagarg <riya723garg@gmail.com>
2026-07-29 20:06:48 +0530

    pinpopup ui

--- frontend/src/components/PinPopup.jsx
+++ frontend/src/components/PinPopup.jsx
@@ -17,16 +17,11 @@ function PinPopup({ pin, onClose }) {
 
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-30 ">
-      <div className="bg-white p-6 rounded-lg w-72">
+      <div className="glass-pinpopup p-6 rounded-lg w-72">
         <div className="flex flex-col gap-2">
-          <button className="btn btn-primary" onClick={handleUpload}> Upload Photos </button>
-          <button
-            className="btn btn-secondary"
-            onClick={handleAddToBucketList}
-            disabled={pin.isFavorite}
-          >
-            {pin.isFavorite ? "Already in Bucket List" : "Add to Bucket List"}
-          </button>
+          <button className="btn bg-[#355653] text-white border-none" onClick={handleUpload}> Upload Photos </button>
+          <button className="btn bg-[#e64e67] text-white border-none" onClick={handleAddToBucketList} disabled={pin.isFavorite}>
+            {pin.isFavorite ? "Already in Bucket List" : "Add to Bucket List"} </button>
         </div>
 
         <button onClick={onClose} className="btn btn-ghost mt-2 w-full"> Close </button>
